d77e1bd8b699d5a58109314f934a329fe880c829
YatFungLoo <looyatfung@gmail.com>
2025-01-17 23:58:20 +0800

    added red black class

--- include/BST.hpp
+++ include/BST.hpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <queue>
 
+// Red black tree colours
+enum Colour { BLACK, RED };
+// Traversing mode
+enum Mode { pre_order, post_order, in_order, level_order };
+
 struct Node {
-  // public:
+    // public:
     int Key;
     Node *Left;
     Node *Right;
-    Node *Parent;
     // Constructors
-    Node(int key) {
-        Key = key;
-        Left = Right = Parent = nullptr;
-    }
+    Node(int key) : Key(key), Left(nullptr), Right(nullptr) {}
 };
 
-// Traversing mode
-enum Mode { pre_order, post_order, in_order, level_order};
+struct Coloured_Node : public Node {
+    // public:
+    bool colour;
+    // Constructors
+    Coloured_Node(int key) : Node(key), colour(BLACK){};
+};
 
 /*
 Basic functions:
@@ -30,7 +35,7 @@ class BST {
   public:
     BST(int key) {
         std::cout << "Creating tree with root node: " << key << std::endl;
-	root_node = new Node(key);
+        root_node = new Node(key);
     };
     void Insert(int key);
     void Remove(int key);
@@ -51,4 +56,27 @@ class BST {
     Node *SimpleSearch(Node *node, int key);
     Node *FindMinValueNode(Node *node);
     Node *FindMaxValueNode(Node *node);
+};
+
+class RBT {
+    Coloured_Node *root_node = nullptr;
+
+  public:
+    RBT(int key) {
+        std::cout << "Creating red-black tree with root node: " << key << std::endl;
+        root_node = new Coloured_Node(key);
+    }
+
+  private:
+    bool TurnRed(Coloured_Node *node);
+    Coloured_Node *RotateLeft(Coloured_Node *node);
+    Coloured_Node *RotateRight(Coloured_Node *node);
+    Coloured_Node *FlipColours(Coloured_Node *node);
+    int size() { return size(root_node); };
+    int size(Coloured_Node *node) {
+        if (node == nullptr) {
+            return 0;
+        }
+        return 0;
+    };
 };
--- src/BST.cpp
+++ src/BST.cpp
@@ -1,5 +1,6 @@
 #include "BST.hpp"
 
+// Class BST
 void BST::Insert(int key) {
     std::cout << "Inserting: " << key << std::endl;
     root_node = InsertCompare(root_node, key);
@@ -166,7 +167,7 @@ void BST::LevelOrderTraverse(Node *node) {
     node_queue.push(node);
 
     while (!node_queue.empty()) {
-	int queue_length = node_queue.size();
+        int queue_length = node_queue.size();
         for (int i = 0; i < queue_length; i++) {
 	    auto dequeue_node = (node_queue.front());
 	    node_queue.pop();
@@ -182,9 +183,9 @@ void BST::LevelOrderTraverse(Node *node) {
 
     // Printer.
     int length = visited.size();
-    for (int i = 0 ; i < length; i++) {
-	std::cout << visited.front()->Key << ", ";
-	visited.pop();
+    for (int i = 0; i < length; i++) {
+        std::cout << visited.front()->Key << ", ";
+        visited.pop();
     }
 }
 
@@ -257,3 +258,17 @@ Node *BST::FindMaxValueNode(Node *node) {
     }
     return node;
 }
+// End of Class BST
+
+// Class RBT
+bool RBT::TurnRed(Coloured_Node *node) {
+    if (node == nullptr) {
+        return false;
+    }
+    return node->colour == RED;
+}
+
+Coloured_Node *RBT::RotateLeft(Coloured_Node *node) { return node; }
+Coloured_Node *RBT::RotateRight(Coloured_Node *node) { return node; }
+Coloured_Node *RBT::FlipColours(Coloured_Node *node) { return node; }
+// End of Class RBT
